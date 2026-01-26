827b9819cf858b04e75a95c98d9055b3360ad2bf
kleitonjatoba <kleitonjatoba@gmail.com>
2026-01-26 09:19:07 +0000

    REESTRUTURA: Adiciona layout persistente ao workflow

---
+++ app/step/layout.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import Layout from "@/components/Layout";
+import WorkflowTracker from "@/components/workflow/WorkflowTracker";
+import { Suspense } from "react";
+
+export default function StepLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <Layout>
+      <Suspense fallback={
+        <div className=\"flex items-center justify-center h-full\"><p>Carregando...</p></div>
+      }>
+        <WorkflowTracker />
+        <div className="pt-8">
+          {children}
+        </div>
+      </Suspense>
+    </Layout>
+  );
+}
